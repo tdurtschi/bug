1876fa56c819620b6fedb1dba43c04e08aeb3ff9
T Durtschi <tdurtschi@users.noreply.github.com>
2019-11-26 20:00:08 -0500

    Change initial game state - walls are out of frame and no 'alive' entities.

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,13 @@ import Victor from "victor";
 
 import App from "./app/components/App";
 import { GameEngine, Game } from './app/game-engine';
-import Bug from './bug/bug';
 import Wall from './wall/wall';
-import Tree from './tree/tree';
 import EntityManager from './app/entity-manager';
 import GameUI from './app/game-ui';
 
 const entities = [
-	new Bug(0, { pos: new Victor(50, 0) }),
-	new Wall(1, { pos: new Victor(-5, 0), size: new Victor(10, 400) }),
-	new Wall(2, { pos: new Victor(395, 0), size: new Victor(10, 400) }),
-	new Tree(3, { pos: new Victor(150, 0) })
+	new Wall(1, { pos: new Victor(-10, 0), size: new Victor(10, 400) }),
+	new Wall(2, { pos: new Victor(400, 0), size: new Victor(10, 400) }),
 ]
 
 const entityManager = new EntityManager(entities)
