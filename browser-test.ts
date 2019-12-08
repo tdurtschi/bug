baedc3521cc0c5d68b685eb7658b67fb1db7d82e
T Durtschi <tdurtschi@users.noreply.github.com>
2019-12-08 13:30:38 -0500

    Fix browser test and reformat it.

--- browser-test.ts
+++ browser-test.ts
@@ -14,11 +14,14 @@ afterEach(async () => {
   await browser.close();
 })
 
-it("Loads the page with no errors", async () => {
-  const page = await browser.newPage();
+it("Loads the page with no errors", async (done) => {
+  const page = await browser.newPage()
   page.on('pageerror', (e: any) => fail(`Encountered exception: ${e}`))
-  await page.goto('http://localhost:9000');
-  console.log("waiting 3 seconds for errors...");
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  console.log("done!");
+
+  await page.goto('http://localhost:9000')
+  console.log("Waiting 3 seconds for errors...")
+  await new Promise((resolve) => setTimeout(resolve, 3000))
+  console.log("All done!")
+
+  done()
 }, TIMEOUT)
