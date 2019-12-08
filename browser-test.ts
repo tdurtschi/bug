fcdc203378250220268726b8a8fa8552b728000e
T Durtschi <tdurtschi@users.noreply.github.com>
2019-12-08 13:19:07 -0500

    Add browser test

---
+++ browser-test.ts
@@ -0,0 +1,24 @@
+import puppeteer, { Browser } from "puppeteer";
+
+const TIMEOUT = 20000
+
+let browser: Browser;
+
+beforeEach(async () => {
+  browser = await puppeteer.launch({
+
+  });
+})
+
+afterEach(async () => {
+  await browser.close();
+})
+
+it("Loads the page with no errors", async () => {
+  const page = await browser.newPage();
+  page.on('pageerror', (e: any) => fail(`Encountered exception: ${e}`))
+  await page.goto('http://localhost:9000');
+  console.log("waiting 3 seconds for errors...");
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  console.log("done!");
+}, TIMEOUT)
