3c24380833c281a5721029f6d21d567a5189be4b
T Durtschi <tdurtschi@users.noreply.github.com>
2019-12-21 00:03:46 -0500

    Update bug renderer to work with angled bugs.

--- src/canvas-ui/renderers/bug/bug-renderer.ts
+++ src/canvas-ui/renderers/bug/bug-renderer.ts
@@ -1,19 +1,38 @@
 import BugUI from "./bug-ui"
 import { fixY } from "../../canvas-helpers"
+import Victor = require("victor")
 
 export default (uiBug: BugUI, ctx: CanvasRenderingContext2D) => {
 	const bug = uiBug.bug
 	const image = uiBug.getImage()
 
-	const { pos, direction, size } = bug.state
+	const { direction, size } = bug.state
+
+	const pos = new Victor(bug.state.pos.x, fixY(ctx, bug.state.pos.y))
 
 	ctx.save()
-	ctx.translate(pos.x, fixY(ctx, pos.y))
-	ctx.translate(0, -size.y)
+	ctx.translate(pos.x, pos.y)
+
 	if (direction.x > 0)
 	{
+		ctx.rotate(-direction.angle())
 		ctx.scale(-1, 1)
+		ctx.drawImage(image, 0, -size.y, size.x, size.y)
+	} else
+	{
+		const newDir = direction.clone().multiplyScalarX(-1)
+
+		ctx.rotate(newDir.angle())
+		ctx.drawImage(image, 0, -size.y, size.x, size.y)
 	}
-	ctx.drawImage(image, 0, 0, size.x, size.y)
+
 	ctx.restore()
+}
+
+// Draws a circle at 0,0. Useful for debugging bug rendering
+const circle = (ctx: CanvasRenderingContext2D, size: number) => {
+	ctx.beginPath();
+	ctx.arc(0, 0, size, 0, 2 * Math.PI, false);
+	ctx.fillStyle = "rgb(0, 0, 255)";
+	ctx.fill();
 }
