077833e380102fce5d67af33b8de58e1b186c638
T Durtschi <tdurtschi@users.noreply.github.com>
2020-02-24 18:42:43 -0500

    fixed up plantago bush a bit

--- src/entities/plant/plantagoBushStruct.ts
+++ src/entities/plant/plantagoBushStruct.ts
@@ -1,37 +1,44 @@
 import { ITreeStruct } from "./ITreeStruct";
 import Victor = require("victor");
 import { PlantagoStruct } from "./plantagoStruct";
-import { randInt } from "../../util";
+import { randInt, randBool, normalRange } from "../../util";
 
 export class PlantagoBushStruct implements ITreeStruct {
-	parent: ITreeStruct;
 	left: ITreeStruct | null;
 	right: ITreeStruct | null;
 	node: Victor;
-	depth: number;
-	maxDepth: number;
-	flip: boolean;
 
-	constructor(depth: number = 1, parent?: ITreeStruct, maxDepth: number = 35) {
-		this.flip = depth % 2 === 0
-		this.node = new Victor(0, 8)
-		this.depth = depth
-		this.parent = parent
-		this.maxDepth = maxDepth
-		this.node.rotateByDeg(this.flip ? -80 : -100)
+	constructor(
+		public depth: number = 1,
+		public parent: ITreeStruct | null = null,
+		private maxDepth: number = 4,
+		private flip: boolean = false,
+		private angle: number = 0,
+		private nodeLength: number = 40
+	) {
+		if (depth == 1)
+		{
+			this.flip = randBool()
+			this.maxDepth = randInt(5, 10)
+			this.angle = normalRange(-15, 15)
+			this.nodeLength = normalRange(30, 60)
+		}
+
+		this.node = new Victor(0, 1).multiplyScalar(this.nodeLength)
+		this.node.rotateByDeg((this.flip ? -80 : -100) + this.angle)
 
 		if (depth < maxDepth)
 		{
-			this.left = this.getNextBranch(this.flip)
-			this.right = this.getNextBranch(!this.flip)
+			this.left = this.flip ? this.branch() : this.leaf()
+			this.right = this.flip ? this.leaf() : this.branch()
+		} else
+		{
+			this.left = this.leaf()
 		}
 	}
 
-	getNextBranch = (bool: boolean): ITreeStruct =>
-		bool
-			? new PlantagoBushStruct(this.depth + 1, this, this.maxDepth)
-			: new PlantagoStruct(this.depth + 1, this, this.maxDepth, (this.flip ? -30 : -160) + randInt(-15, 15))
-
+	private branch = () => new PlantagoBushStruct(this.depth + 1, this, this.maxDepth, !this.flip, this.angle, this.nodeLength)
+	private leaf = () => new PlantagoStruct(this.depth + 1, this, this.maxDepth, (this.flip ? -30 : -160) + randInt(-15, 15) + this.angle)
 
 	update = () => {
 	};
