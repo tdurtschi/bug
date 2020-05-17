36ea88cca4c7c7c8fed2cbb42d8eff953e09af1e
Teagan Durtschi <tdurtschi@pivotal.io>
2020-05-17 10:18:26 -0400

    Getting rid of a bunch of semicolons. Small refactor in PlantRenderer

--- src/canvas-ui/renderers/tree/plant-renderer.ts
+++ src/canvas-ui/renderers/tree/plant-renderer.ts
@@ -1,79 +1,80 @@
-import { fixY } from "../../canvas-helpers";
-import Plant from "../../../entities/plant/plant";
-import { ITreeStruct } from "../../../entities/plant/ITreeStruct";
-import { PlantagoStruct } from "../../../entities/plant/plantagoStruct";
+import { fixY } from "../../canvas-helpers"
+import Plant from "../../../entities/plant/plant"
+import { ITreeStruct } from "../../../entities/plant/ITreeStruct"
+import { PlantagoStruct } from "../../../entities/plant/plantagoStruct"
 
-const leafReference = require("./Leaf.png");
-const leaf = new Image();
-leaf.src = leafReference;
+const leafReference = require("./Leaf.png")
+const leaf = new Image()
+leaf.src = leafReference
 
-const preRenderedPlants: OffscreenCanvas[] = [];
+const preRenderedPlants: OffscreenCanvas[] = []
 
 type EitherCanvasContext =
   | OffscreenCanvasRenderingContext2D
-  | CanvasRenderingContext2D;
+  | CanvasRenderingContext2D
 
 export default (plant: Plant, ctx: CanvasRenderingContext2D) => {
   if (preRenderedPlants[plant.id]) {
-    drawPreRenderedPlant(plant, ctx);
+    drawPreRenderedPlant(plant, ctx)
   } else {
-    const plantCanvas = new OffscreenCanvas(
-      ctx.canvas.width,
-      ctx.canvas.height
-    );
-    preRenderedPlants[plant.id] = plantCanvas;
-    drawPlantToCanvas(plant, plantCanvas.getContext("2d"));
+    const plantCanvas = new OffscreenCanvas(ctx.canvas.width, ctx.canvas.height)
+    preRenderedPlants[plant.id] = plantCanvas
+    drawPlantToCanvas(plant, plantCanvas.getContext("2d"))
   }
-};
+}
 
 const drawPlantToCanvas = (plant: Plant, ctx: EitherCanvasContext) => {
-  const { pos } = plant;
-  ctx.save();
-  ctx.strokeStyle = "#a56a27";
-  ctx.lineWidth = 10;
-  ctx.translate(pos.x, fixY(ctx, pos.y));
-  renderTree(plant.graph, ctx);
-  ctx.restore();
-};
+  const { pos } = plant
+  ctx.save()
+  ctx.strokeStyle = "#a56a27"
+  ctx.lineWidth = 10
+  ctx.translate(pos.x, fixY(ctx, pos.y))
+  renderTree(plant.graph, ctx)
+  ctx.restore()
+}
 
 const drawPreRenderedPlant = (plant: Plant, ctx: CanvasRenderingContext2D) => {
-  const rendered = preRenderedPlants[plant.id];
-  ctx.drawImage(rendered, 0, 0, ctx.canvas.width, ctx.canvas.height);
-};
+  const rendered = preRenderedPlants[plant.id]
+  ctx.drawImage(rendered, 0, 0, ctx.canvas.width, ctx.canvas.height)
+}
 
 const renderTree = (root: ITreeStruct, ctx: EitherCanvasContext) => {
   if (root instanceof PlantagoStruct) {
-    renderPlantago(root, ctx);
+    renderLeaf(root, ctx)
   } else {
-    const x = Math.floor(root.node.x),
-      y = Math.floor(root.node.y);
-    ctx.moveTo(0, 0);
-    ctx.lineTo(x, -y);
-    ctx.stroke();
-
-    ctx.save();
-    ctx.translate(x, -y);
-    if (root.left) {
-      renderTree(root.left, ctx);
-    }
-    if (root.right) {
-      renderTree(root.right, ctx);
-    }
-    ctx.restore();
+    renderBranch(root, ctx)
   }
-};
+}
 
-const renderPlantago = (root: ITreeStruct, ctx: EitherCanvasContext) => {
+const renderBranch = (root: ITreeStruct, ctx: EitherCanvasContext) => {
   const x = Math.floor(root.node.x),
-    y = Math.floor(root.node.y);
+    y = Math.floor(root.node.y)
+  ctx.moveTo(0, 0)
+  ctx.lineTo(x, -y)
+  ctx.stroke()
 
-  ctx.save();
-  if (window.DEBUG) {
-    ctx.moveTo(0, 0);
-    ctx.lineTo(x, -y);
-    ctx.stroke();
+  ctx.save()
+  ctx.translate(x, -y)
+  if (root.left) {
+    renderTree(root.left, ctx)
   }
-  ctx.rotate(-root.node.angle() + Math.PI / 2);
-  ctx.drawImage(leaf, -24, -173, 42, 178);
-  ctx.restore();
-};
+  if (root.right) {
+    renderTree(root.right, ctx)
+  }
+  ctx.restore()
+}
+
+const renderLeaf = (root: ITreeStruct, ctx: EitherCanvasContext) => {
+  const x = Math.floor(root.node.x),
+    y = Math.floor(root.node.y)
+
+  ctx.save()
+  if (window.DEBUG) {
+    ctx.moveTo(0, 0)
+    ctx.lineTo(x, -y)
+    ctx.stroke()
+  }
+  ctx.rotate(-root.node.angle() + Math.PI / 2)
+  ctx.drawImage(leaf, -24, -173, 42, 178)
+  ctx.restore()
+}
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import Victor from "victor";
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+import Victor from "victor"
 
-import App from "./app/components/App";
-import { GameEngine } from "./core/game-engine";
-import Wall from "./entities/wall/wall";
-import EntityManager from "./core/entity-manager";
-import CanvasUI from "./canvas-ui/canvas-ui";
-import "./app/app.scss";
-import BugFactory from "./entities/bug/bugFactory";
-import { generateId } from "./core/id-generator";
-import Spontaneous from "./core/spontaneous";
-import { range } from "./util";
-import PlantFactory from "./entities/plant/plantFactory";
+import App from "./app/components/App"
+import { GameEngine } from "./core/game-engine"
+import Wall from "./entities/wall/wall"
+import EntityManager from "./core/entity-manager"
+import CanvasUI from "./canvas-ui/canvas-ui"
+import "./app/app.scss"
+import BugFactory from "./entities/bug/bugFactory"
+import { generateId } from "./core/id-generator"
+import Spontaneous from "./core/spontaneous"
+import { range } from "./util"
+import PlantFactory from "./entities/plant/plantFactory"
 
-const HEIGHT = window.innerHeight;
-const WIDTH = window.innerWidth;
+const HEIGHT = window.innerHeight
+const WIDTH = window.innerWidth
 
-console.info("Starting Bug app...");
+console.info("Starting Bug app...")
 
 const entities = [
   new Wall(generateId(), {
@@ -28,22 +28,22 @@ const entities = [
     pos: new Victor(WIDTH, 0),
     size: new Victor(10, HEIGHT),
   }),
-];
+]
 
-const entityManager = new EntityManager(entities);
+const entityManager = new EntityManager(entities)
 
 const game = new GameEngine({
   gameUI: new CanvasUI("bug-ui", entityManager),
   entityManager,
-});
+})
 
 const spontaneous = new Spontaneous(
   () => range(8000, 4000),
   () => range(10, 4)
-).get;
+).get
 
-const bugFactory = new BugFactory(generateId, spontaneous);
-const treeFactory = new PlantFactory(generateId, spontaneous);
+const bugFactory = new BugFactory(generateId, spontaneous)
+const treeFactory = new PlantFactory(generateId, spontaneous)
 
 const appProps = {
   game,
@@ -52,12 +52,12 @@ const appProps = {
   bugFactory,
   treeFactory,
   entityManager, // TODO - Reduce # of components holding ref to this.
-};
+}
 
 ReactDOM.render(
   React.createElement(App, appProps),
   document.getElementById("root")
-);
+)
 
-(window as any).Victor = Victor;
-window.DEBUG = false;
+;(window as any).Victor = Victor
+window.DEBUG = false
