4d1d34be16aaf08ba475278c1fa7f8191e7d700e
Teagan Durtschi <tdurtschi@pivotal.io>
2020-06-15 15:54:54 -0400

    Simplify bug behavior helper functions. Some weird linting

--- src/entities/bug/behaviors/climb.ts
+++ src/entities/bug/behaviors/climb.ts
@@ -1,118 +1,134 @@
-import { BugBehavior } from "./BugBehavior";
-import { walk } from "./walk";
-import Bug from "../bug";
-import { ITreeStruct } from "../../plant/ITreeStruct";
-import { randBool, randChance, randFromWeighted } from "../../../util";
-import Victor = require("victor");
-import Plant from "../../plant/plant";
-import { TurnAround } from "./turnAround";
-import { PlantagoBushStruct } from "../../plant/plantagoBushStruct";
+import { BugBehavior } from "./BugBehavior"
+import { walk } from "./walk"
+import Bug from "../bug"
+import { ITreeStruct } from "../../plant/ITreeStruct"
+import { randBool, randChance, randFromWeighted, randInt } from "../../../util"
+import Victor = require("victor")
+import Plant from "../../plant/plant"
+import { TurnAround } from "./turnAround"
+import { PlantagoBushStruct } from "../../plant/plantagoBushStruct"
+import { Pause } from "./pause"
 
 export class Climb extends BugBehavior {
+  public do(): void {
+    const bug = this.bug
+    if (!bug.climbingOn) {
+      bug.finishBehavior()
+      return
+    }
 
-	public do(): void {
-		const bug = this.bug
-		if (!bug.climbingOn) {
-			bug.finishBehavior()
-			return
-		}
+    const currentBranch = bug.climbingOn.branch
+    const branchPosition = bug.climbingOn.plant.getAbsolutePos(currentBranch)
+    const branchOffset = bug.pos.clone().subtract(branchPosition)
 
-		const currentBranch = bug.climbingOn.branch //bug?
-		const branchPosition = bug.climbingOn.plant.getAbsolutePos(currentBranch)
-		const branchOffset = bug.pos.clone().subtract(branchPosition)
+    const angleBetweenBranchAndDirection = Math.abs(
+      currentBranch.node.direction() - bug.direction.direction()
+    )
+    const isGoingDown = angleBetweenBranchAndDirection > 3
+    if (isGoingDown) {
+      const directionDelta = Math.abs(
+        branchOffset.direction() - currentBranch.node.direction()
+      )
+      if (directionDelta > 0.3) {
+        if (currentBranch.parent) {
+          bug.climbingOn.branch = currentBranch.parent
+          bug.pos = bug.climbingOn.plant
+            .getAbsolutePos(bug.climbingOn.branch)
+            .add(bug.climbingOn.branch.node)
+          bug.direction = bug.climbingOn.branch.node
+            .clone()
+            .norm()
+            .multiplyScalar(-1)
+        } else {
+          endClimbing(bug)
+        }
+      }
+    } else if (branchOffset.magnitude() >= currentBranch.node.magnitude()) {
+      const nextBranch = getNextBranch(currentBranch)
 
-		const angleBetweenBranchAndDirection = Math.abs(currentBranch.node.direction() - bug.direction.direction())
-		const isGoingDown = angleBetweenBranchAndDirection > 3
-		if (isGoingDown) {
-			const directionDelta = Math.abs(branchOffset.direction() - currentBranch.node.direction())
-			if (directionDelta > 0.3) {
-				if (currentBranch.parent) {
-					bug.climbingOn.branch = currentBranch.parent
-					bug.pos = bug.climbingOn.plant.getAbsolutePos(bug.climbingOn.branch)
-						.add(bug.climbingOn.branch.node)
-					bug.direction = bug.climbingOn.branch.node.clone().norm().multiplyScalar(-1)
-				} else {
-					endClimbing(bug)
-				}
-			}
-		} else if (branchOffset.magnitude() >= currentBranch.node.magnitude()) {
-			const nextBranch = getNextBranch(currentBranch)
+      if (nextBranch) {
+        climbBranch(bug, nextBranch)
+      } else {
+        bug.queueBehavior(new TurnAround(bug))
+        bug.queueBehavior(new Climb(bug))
+        bug.finishBehavior()
+      }
+    }
 
-			if (nextBranch) {
-				climbBranch(bug, nextBranch)
-			} else {
-				bug.queueBehavior(new TurnAround(bug))
-				bug.queueBehavior(new Climb(bug))
-				bug.finishBehavior()
-			}
-		}
-
-		if (randFromWeighted([394, 6])) {
-			bug.finishBehavior()
-		}
-		else {
-			walk(bug)
-		}
-	}
+    if (randFromWeighted([394, 6])) {
+      bug.finishBehavior()
+    } else {
+      walk(bug)
+    }
+  }
 }
 
 const getNextBranch = (currentBranch: ITreeStruct) => {
-	if (randChance(4, 5)) {
-		if (currentBranch.left && currentBranch.left.node instanceof PlantagoBushStruct) {
-			return currentBranch.left
-		}
-		if (currentBranch.right && currentBranch.right.node instanceof PlantagoBushStruct) {
-			return currentBranch.right
-		}
-	}
+  if (randFromWeighted([4, 1])) {
+    if (
+      currentBranch.left &&
+      currentBranch.left.node instanceof PlantagoBushStruct
+    ) {
+      return currentBranch.left
+    }
+    if (
+      currentBranch.right &&
+      currentBranch.right.node instanceof PlantagoBushStruct
+    ) {
+      return currentBranch.right
+    }
+  }
 
-	return randBool() && currentBranch.left
-		? currentBranch.left
-		: currentBranch.right
-			? currentBranch.right
-			: undefined
+  return randBool() && currentBranch.left
+    ? currentBranch.left
+    : currentBranch.right
 }
 
 export const beginClimbing = (bug: Bug, tree: Plant) => {
-	bug.direction = tree.graph.node.clone().norm()
-	bug.pos = new Victor(tree.pos.x, 0).add(bug.direction.clone().multiplyScalar(bug.size.x))
-	bug.climbingOn = {
-		plant: tree,
-		branch: tree.graph
-	}
+  bug.direction = tree.graph.node.clone().norm()
+  bug.pos = new Victor(tree.pos.x, 0).add(
+    bug.direction.clone().multiplyScalar(bug.size.x)
+  )
+  bug.climbingOn = {
+    plant: tree,
+    branch: tree.graph,
+  }
 
-	bug.zIndexChanged.next()
+  bug.zIndexChanged.next()
 }
 
 const endClimbing = (bug: Bug) => {
-	bug.direction = new Victor(randBool() ? 1 : -1, 0)
-	const sizeOffset = bug.direction.x > 0
-		? (bug.size.x + 1)
-		: -(bug.size.x + 1)
-	const newX = bug.climbingOn.plant.pos.x + sizeOffset
-	bug.pos = new Victor(newX, bug.climbingOn.plant.pos.y)
-	bug.climbingOn = undefined
+  bug.direction = new Victor(randBool() ? 1 : -1, 0)
+  const sizeOffset = bug.direction.x > 0 ? bug.size.x + 1 : -(bug.size.x + 1)
+  const newX = bug.climbingOn.plant.pos.x + sizeOffset
+  bug.pos = new Victor(newX, bug.climbingOn.plant.pos.y)
+  bug.climbingOn = undefined
 
-	bug.zIndexChanged.next()
-	bug.finishBehavior()
+  bug.zIndexChanged.next()
+  bug.finishBehavior()
 }
 
-const climbBranch = (bug: Bug, branch: ITreeStruct, direction: Direction = Direction.UP) => {
-	let offset: Victor;
-	if (direction === Direction.UP) {
-		offset = new Victor(bug.size.x / 3, 0)
-			.rotate(branch.node.direction())
-	} else {
-		offset = branch.node
-	}
+const climbBranch = (
+  bug: Bug,
+  branch: ITreeStruct,
+  direction: Direction = Direction.UP
+) => {
+  let offset: Victor
+  if (direction === Direction.UP) {
+    offset = new Victor(bug.size.x / 3, 0).rotate(branch.node.direction())
+  } else {
+    offset = branch.node
+  }
 
-	bug.climbingOn.branch = branch
-	bug.pos = bug.climbingOn.plant
-		.getAbsolutePos(branch)
-		.add(offset)
-	bug.direction = branch.node.clone()
-		.norm()
-		.multiplyScalar(direction === Direction.UP ? 1 : -1)
+  bug.climbingOn.branch = branch
+  bug.pos = bug.climbingOn.plant.getAbsolutePos(branch).add(offset)
+  bug.direction = branch.node
+    .clone()
+    .norm()
+    .multiplyScalar(direction === Direction.UP ? 1 : -1)
 }
 
-enum Direction { UP, DOWN }
+enum Direction {
+  UP,
+  DOWN,
+}
--- src/entities/bug/bug.ts
+++ src/entities/bug/bug.ts
@@ -11,105 +11,116 @@ import { randInt, normalRange, randFromWeighted } from "../../util"
 import { Climb } from "./behaviors/climb"
 
 export interface BugState extends EntityState {
-	direction: Victor
-	speed: number
-	spontaneous: () => boolean
-	climbingOn?: IClimbingOn
+  direction: Victor
+  speed: number
+  spontaneous: () => boolean
+  climbingOn?: IClimbingOn
 }
 
 export interface IClimbingOn {
-	plant: Plant
-	branch: ITreeStruct
+  plant: Plant
+  branch: ITreeStruct
 }
 
 class Bug implements Entity, BugState {
-	direction: Victor
-	speed: number
-	spontaneous: () => boolean
-	climbingOn?: IClimbingOn
-	pos: Victor
-	size: Victor
-	id: number
-	state: BugState
-	updateSpeed: number = 4
-	zIndexChanged: Subject<void> = new Subject<void>()
-	behaviorQueue: BugBehavior[] = []
+  direction: Victor
+  speed: number
+  spontaneous: () => boolean
+  climbingOn?: IClimbingOn
+  pos: Victor
+  size: Victor
+  id: number
+  state: BugState
+  updateSpeed: number = 4
+  zIndexChanged: Subject<void> = new Subject<void>()
+  behaviorQueue: BugBehavior[] = []
 
-	constructor(id?: number, initialState?: Partial<BugState>) {
-		this.id = id ? id : 0
+  constructor(id?: number, initialState?: Partial<BugState>) {
+    this.id = id ? id : 0
 
-		Object.assign(
-			this,
-			{
-				//Position: the front of the bug at foot level
-				pos: new Victor(0, 0),
-				size: new Victor(30, 20),
-				direction: new Victor(1, 0),
-				speed: 1,
-				behaviorQueue: [],
-				spontaneous: () => false
-			}, initialState)
+    Object.assign(
+      this,
+      {
+        //Position: the front of the bug at foot level
+        pos: new Victor(0, 0),
+        size: new Victor(30, 20),
+        direction: new Victor(1, 0),
+        speed: 1,
+        behaviorQueue: [],
+        spontaneous: () => false,
+      },
+      initialState
+    )
 
-		this.queueBehavior(new Pause(this, randInt(25, 40)));
-	}
+    this.queueBehavior(new Pause(this, randInt(25, 40)))
+  }
 
-	public update(inputs: Entity[] = []): Bug {
-		if (this.spontaneous()) {
-			this.queueBehavior(new TurnAround(this))
-		}
+  public update(inputs: Entity[] = []): Bug {
+    if (this.spontaneous()) {
+      this.queueBehavior(new TurnAround(this))
+    }
 
-		if (this.behaviorQueue && this.behaviorQueue.length > 0) {
-			this.behaviorQueue[0].do(inputs)
-		} else {
-			const next = this.getNextBehavior()
-			next.do(inputs)
-			this.queueBehavior(next)
-		}
+    if (this.behaviorQueue && this.behaviorQueue.length > 0) {
+      this.behaviorQueue[0].do(inputs)
+    } else {
+      const next = this.getNextBehavior()
+      next.do(inputs)
+      this.queueBehavior(next)
+    }
 
-		return this
-	}
+    return this
+  }
 
-	getNextBehavior(currentBehavior?: BugBehavior): BugBehavior {
-		if (currentBehavior) {
-			if (currentBehavior instanceof Pause) {
-				enum Choices { PAUSE, TURN_AROUND, WALK }
-				const action = (randFromWeighted([3, 4, 2]) as Choices)
-				switch (action) {
-					case Choices.PAUSE: return new Pause(this, randInt(10, 20));
-					case Choices.TURN_AROUND: return new TurnAround(this);
-					default: return this.climbingOn ? new Climb(this) : new GroundWalk(this, normalRange(60, 140));
-				}
-			} else if (currentBehavior instanceof TurnAround) {
-				return new Pause(this, 10)
-			} else if (currentBehavior instanceof Climb) {
-				return new Pause(this, randInt(20, 50))
-			} else {
-				return new Pause(this, 10)
-			}
-		} else {
-			return this.climbingOn ? new Climb(this)
-				: randFromWeighted([1, 3]) ? new Pause(this, randInt(20, 40))
-					: new GroundWalk(this, normalRange(60, 140))
-		}
-	}
+  getNextBehavior(currentBehavior?: BugBehavior): BugBehavior {
+    if (currentBehavior) {
+      if (currentBehavior instanceof Pause) {
+        enum Choices {
+          PAUSE,
+          TURN_AROUND,
+          WALK,
+        }
+        const action = randFromWeighted([3, 4, 2]) as Choices
+        switch (action) {
+          case Choices.PAUSE:
+            return new Pause(this, randInt(10, 20))
+          case Choices.TURN_AROUND:
+            return new TurnAround(this)
+          default:
+            return this.climbingOn
+              ? new Climb(this)
+              : new GroundWalk(this, normalRange(60, 140))
+        }
+      } else if (currentBehavior instanceof TurnAround) {
+        return new Pause(this, 10)
+      } else if (currentBehavior instanceof Climb) {
+        return new Pause(this, randInt(20, 50))
+      } else {
+        return new Pause(this, randInt(0, 100))
+      }
+    } else if (this.climbingOn) {
+      return new Climb(this)
+    } else {
+      return randFromWeighted([1, 3])
+        ? new Pause(this, randInt(20, 40))
+        : new GroundWalk(this, normalRange(60, 140))
+    }
+  }
 
-	public queueBehavior = (behavior: BugBehavior) => {
-		if (behavior === undefined) {
-			throw new Error("Behavior Undefined");
-		}
+  public queueBehavior = (behavior: BugBehavior) => {
+    if (behavior === undefined) {
+      throw new Error("Behavior Undefined")
+    }
 
-		this.behaviorQueue.push(behavior);
-	}
+    this.behaviorQueue.push(behavior)
+  }
 
-	public finishBehavior() {
-		if (this.behaviorQueue.length == 1) {
-			const nextBehavior = this.getNextBehavior(this.behaviorQueue[0])
-			this.behaviorQueue.shift()
-			this.queueBehavior(nextBehavior);
-		} else {
-			this.behaviorQueue.shift()
-		}
-	}
+  public finishBehavior() {
+    if (this.behaviorQueue.length == 1) {
+      this.behaviorQueue.shift()
+    } else {
+      this.behaviorQueue.shift()
+    }
+  }
 }
 
 export default Bug
