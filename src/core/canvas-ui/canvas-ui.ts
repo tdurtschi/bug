071942c0942f712d9f03234f7b7e584fffa3e732
Teagan Durtschi <teagan@durtschi.com>
2023-02-09 18:14:05 -0600

    Only add points by clicking during debug mode

--- src/core/canvas-ui/canvas-ui.ts
+++ src/core/canvas-ui/canvas-ui.ts
@@ -18,83 +18,88 @@ export interface IGameUI {
 }
 
 class CanvasUI implements IGameUI {
-	ctx: CanvasRenderingContext2D
-	isPaused: boolean = false
-	frame: number = 0
-	lastClickX: number | undefined;
-	lastClickY: number | undefined;
+  ctx: CanvasRenderingContext2D;
+  isPaused: boolean = false;
+  frame: number = 0;
+  lastClickX: number | undefined;
+  lastClickY: number | undefined;
 
-	constructor(
-		private target: string,
-		private entityManager: IEntityManager) {
-	}
+  constructor(private target: string, private entityManager: IEntityManager) {}
 
-	public start = (): void => {
-		const canvas = this.createCanvas();
-		canvas.addEventListener("click", (e) => this.onCanvasClick(e));
-		this.ctx = canvas.getContext("2d")
+  public start = (): void => {
+    const canvas = this.createCanvas();
+    canvas.addEventListener("click", (e) => this.onCanvasClick(e));
+    this.ctx = canvas.getContext("2d");
 
-		this.beginLoop = this.beginLoop.bind(this)
+    this.beginLoop = this.beginLoop.bind(this);
 
-		this.beginLoop(0)
-	}
+    this.beginLoop(0);
+  };
 
-	public render = (): void => {
-		clearCanvas(this.ctx)
-		this.entityManager.getEntities().forEach(entity => {
-			this.renderEntity(entity, this.ctx)
-		})
-	}
+  public render = (): void => {
+    clearCanvas(this.ctx);
+    this.entityManager.getEntities().forEach((entity) => {
+      this.renderEntity(entity, this.ctx);
+    });
+  };
 
-	public togglePause = (): void => {
-		this.isPaused = !this.isPaused;
-	}
+  public togglePause = (): void => {
+    this.isPaused = !this.isPaused;
+  };
 
-	beginLoop(timeMs: number) {
-		if (!this.isPaused) {
-			this.render()
-		}
+  beginLoop(timeMs: number) {
+    if (!this.isPaused) {
+      this.render();
+    }
 
-		window.requestAnimationFrame(this.beginLoop)
-	}
+    window.requestAnimationFrame(this.beginLoop);
+  }
 
-	createCanvas() {
-		const container = document.querySelector(this.target) as HTMLDivElement
+  createCanvas() {
+    const container = document.querySelector(this.target) as HTMLDivElement;
 
-		const canvas = document.createElement("canvas");
-		canvas.height = container.clientHeight
-		canvas.width = container.clientWidth
-		container.appendChild(canvas)
+    const canvas = document.createElement("canvas");
+    canvas.height = container.clientHeight;
+    canvas.width = container.clientWidth;
+    container.appendChild(canvas);
 
-		return canvas
-	}
+    return canvas;
+  }
 
-	renderEntity(entity: Entity, ctx: CanvasRenderingContext2D) {
-		if (entity instanceof Bug) {
-			bugRenderer(entity as Bug, ctx)
-		}
-		else if (entity instanceof Wall) {
-			wallRenderer((entity as Wall), ctx)
-		}
-		else if (entity instanceof Plant) {
-			plantRenderer((entity as Plant), ctx)
-		} else if (entity instanceof Point) {
-			pointRenderer((entity as Point), ctx)
-		}
-	}
+  renderEntity(entity: Entity, ctx: CanvasRenderingContext2D) {
+    if (entity instanceof Bug) {
+      bugRenderer(entity as Bug, ctx);
+    } else if (entity instanceof Wall) {
+      wallRenderer(entity as Wall, ctx);
+    } else if (entity instanceof Plant) {
+      plantRenderer(entity as Plant, ctx);
+    } else if (entity instanceof Point) {
+      pointRenderer(entity as Point, ctx);
+    }
+  }
 
-	onCanvasClick(e: any) {
-		const {x, y} = {x: e.x - 200, y: e.y};
-		const newY = fixY(this.ctx.canvas.height, y);
-		if(this.lastClickX) {
-			console.log("Diff from last click:")
-			console.log({x: x - this.lastClickX, y: newY - this.lastClickY})
-		}
-		console.log({x, y: newY});
-		this.entityManager.addEntity(new Point(generateId(), {pos: new Victor(x + 200, newY), size: new Victor(3, 3)}));
-		this.lastClickX = x;
-		this.lastClickY = newY;
-	}
+  onCanvasClick(e: any) {
+    if (window.DEBUG) {
+      const { x, y } = { x: e.x - 200, y: e.y };
+      const newY = fixY(this.ctx.canvas.height, y);
+     
+	  if (this.lastClickX) {
+        console.log("Diff from last click:");
+        console.log({ x: x - this.lastClickX, y: newY - this.lastClickY });
+      }
+      console.log({ x, y: newY });
+      
+	  this.entityManager.addEntity(
+        new Point(generateId(), {
+          pos: new Victor(x + 200, newY),
+          size: new Victor(3, 3),
+        })
+      );
+
+      this.lastClickX = x;
+      this.lastClickY = newY;
+    }
+  }
 }
 
 export default CanvasUI
